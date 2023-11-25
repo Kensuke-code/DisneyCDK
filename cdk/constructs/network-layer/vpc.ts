c561f7c0cecdae9fb32c9171322a16a23d3eff6a
Kensuke-code <kenzo71719083@gmail.com>
2023-11-25 13:48:15 +0900

    インスタンスタイプ小さすぎたので増強

--- cdk/constructs/network-layer/vpc.ts
+++ cdk/constructs/network-layer/vpc.ts
@@ -32,6 +32,10 @@ export class VPC extends Construct {
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80)
     );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443)
+    );
 
     // UserData
     const userData = ec2.UserData.forLinux({ shebang: '#!/bin/bash -x' })
@@ -50,7 +54,7 @@ export class VPC extends Construct {
     // EC2インスタンス作成
     const instance = new ec2.Instance(this, 'DisneyGachaInstance', {
       vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
       machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
       userData: userData,
       securityGroup: securityGroup,
